acd8ba230a6b305d98458d790082636f242f1050
KevinTss <kevin.tassi@gmail.com>
2021-10-30 19:14:20 +0200

    fix create class corm header style

--- components/classes/create-class-drawer/CreateClassDrawer.jsx
+++ components/classes/create-class-drawer/CreateClassDrawer.jsx
@@ -1,15 +1,18 @@
 import { Dialog } from '../../ui';
 import CreateClassForm from '../create-class-form';
 import { Title, Text } from '../../../styles/GlobalStyle';
+import { Header } from './style';
 
 const CreateClassDrawer = ({ isOpen, onClose }) => (
   <Dialog isOpen={isOpen} onClose={onClose} type='aside'>
-    <Title>Create a new recurring class</Title>
-    <Text>
-      {
-        "A class is a recurring event which will appear on the calendar view. This will allow dancers to join <b>event</b>. An event doesn't exist since at least one dancer join."
-      }
-    </Text>
+    <Header>
+      <Title>Create a new recurring class</Title>
+      <Text>
+        {`A class is a recurring event which will appear on the calendar view. This will allow dancers to join `}
+        <b>event</b>
+        {`. An event doesn't exist since at least one dancer join.`}
+      </Text>
+    </Header>
     <CreateClassForm onClose={onClose} />
   </Dialog>
 );
