09666542e3968a90f747d3d39a8070d97991d1d8
Ordinateur <kevin.tassi@gmail.com>
2022-03-17 20:59:38 +0100

    Fix style

--- components/event/calendar/week-day/event/add-dancer-drawer/style.js
+++ components/event/calendar/week-day/event/add-dancer-drawer/style.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const DancersContainer = styled.div`
   display: grid;
   grid-template-rows: 1fr 1fr;
-  // gap: 10px;
 `;
 
 export const FullNameContainer = styled.div`
