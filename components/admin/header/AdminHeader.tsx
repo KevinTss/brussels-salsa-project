85a27a000172baca477fa3a30e60fe29a6218c04
KevinTss <kevin.tassi@gmail.com>
2022-05-01 15:18:49 +0200

    Fix breadcrumbs

--- components/admin/header/AdminHeader.tsx
+++ components/admin/header/AdminHeader.tsx
@@ -1,17 +1,43 @@
 import { Avatar, Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
-import { useAuth } from '../../../hooks';
+import { useRouter } from 'next/router'
 
 import { Container, Left, Right } from './styles'
+import { useAuth } from '../../../hooks';
+import { capitalize } from '../../../utils';
+import { RoutePaths } from '../../../types';
+
+function getLastBreadcrumbLabel(path: string): string {
+  const [, , lastPath] = path.split('/')
+
+  return capitalize(lastPath)
+}
+function getLastBreadcrumbHref(path: string): RoutePaths {
+  const [, , lastPath] = path.split('/')
+
+  const links: Record<string, RoutePaths> = {
+    users: RoutePaths.ADMIN_USERS,
+    calendar: RoutePaths.ADMIN_CALENDAR,
+    classe: RoutePaths.ADMIN_CLASSES,
+  }
+
+  return links[lastPath]
+}
 
 const AdminHeader = () => {
   const { currentUser } = useAuth()
+  const { pathname } = useRouter()
 
   return (
     <Container>
       <Left>
         <Breadcrumb>
           <BreadcrumbItem>
-            <BreadcrumbLink href='/admin'>Admin dashboard</BreadcrumbLink>
+            <BreadcrumbLink>Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href={getLastBreadcrumbHref(pathname)}>
+              {getLastBreadcrumbLabel(pathname)}
+            </BreadcrumbLink>
           </BreadcrumbItem>
         </Breadcrumb>
       </Left>
--- types/routes.ts
+++ types/routes.ts
@@ -2,4 +2,7 @@ export enum RoutePaths {
   HOME = '/',
   ONBOARDING = '/onboarding',
   SETTINGS = '/settings',
+  ADMIN_CALENDAR = '/admin/calendar',
+  ADMIN_CLASSES = '/admin/classes',
+  ADMIN_USERS = '/admin/users',
 }
